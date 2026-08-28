dc3562c06f25b97e56cf77c71d8ea1a62adf9ddf
lstamellos <lstamellos@gmail.com>
2026-08-28 05:04:02 +0300

    Fix server editor hook assertions

--- tests/ServerAwareDatasetEditorTest.php
+++ tests/ServerAwareDatasetEditorTest.php
@@ -15,8 +15,8 @@ final class ServerAwareDatasetEditorTest extends TestCase {
 
         $page = file_get_contents( $this->root . '/src/Admin/DatasetEditorPage.php' );
         $this->assertStringContainsString( "get_plugin_page_hookname( 'viswiz-datasets', 'viswiz' )", $page );
-        $this->assertStringContainsString( "remove_action( $hook, array( Admin::class, 'datasets_page' ) )", $page );
-        $this->assertStringContainsString( "add_action( $hook, array( self::class, 'page' ) )", $page );
+        $this->assertStringContainsString( 'remove_action( $hook, array( Admin::class, \'datasets_page\' ) )', $page );
+        $this->assertStringContainsString( 'add_action( $hook, array( self::class, \'page\' ) )', $page );
         $this->assertStringNotContainsString( "remove_submenu_page( 'viswiz', 'viswiz-datasets' )", $page );
         $this->assertStringNotContainsString( "add_submenu_page(\n            'viswiz'", $page );
         $this->assertStringContainsString( 'data-viswiz-server-editor="1"', $page );
