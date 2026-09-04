4e39536500bb3a7c32b46b860beb4d21ddd5e8fc
lstamellos <lstamellos@gmail.com>
2026-09-04 03:08:53 +0300

    Bump spreadsheet release assertion to 2.0.35

--- tests/SpreadsheetEditorTest.php
+++ tests/SpreadsheetEditorTest.php
@@ -66,8 +66,8 @@ final class SpreadsheetEditorTest extends TestCase {
 
     public function test_spreadsheet_release_keeps_database_schema_version(): void {
         $plugin = file_get_contents( $this->root . '/viswiz.php' );
-        self::assertStringContainsString( 'Version: 2.0.34', $plugin );
-        self::assertStringContainsString( "define( 'VISWIZ_VERSION', '2.0.34' );", $plugin );
+        self::assertStringContainsString( 'Version: 2.0.35', $plugin );
+        self::assertStringContainsString( "define( 'VISWIZ_VERSION', '2.0.35' );", $plugin );
         self::assertStringContainsString( "define( 'VISWIZ_DB_VERSION', 20000 );", $plugin );
     }
 }
